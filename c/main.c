17b0a5a1877f8dd29230d473239e4591304e270d
lin <gamecoder@qq.com>
2017-03-31 10:09:25 +0800

    update 注释

--- c/main.c
+++ c/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "ipLocation.h"
-//ÒýÓÃ
+
 int main() {
 	char result[128];
 	initLocation("../ip.dat");
 	findLocation("255.255.255.255", result);
-	printf("%s", result); //ip.dat ip¹éÊôµØÊÇutf-8±àÂë
+	printf("%s", result); //utf-8
 	return 0;
 }
