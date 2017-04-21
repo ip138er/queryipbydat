a5ffe773644c73e37dbe941195dee5ee9d482e15
吴志华 <415694797@qq.com>
2017-04-21 17:53:22 +0800

    updated 完成php版的ip地址查询

--- php/ipLocation.php
+++ php/ipLocation.php
@@ -13,21 +13,26 @@ class Ip138{
     private static $ip     = NULL;
     private static $db     = NULL;
     private static $offset = NULL;
-    private static $index  = NULL;
-    private static $cached = array();
+    private static $index  = array();
+    private static $textData = NULL;
+    private static $idx_start = NULL;
+    private static $total = 0;
+    private static $ipEndAddr = array();
+    private static $textOffset = array();
+    private static $textLen = array();
+
 
     public static function getInstance(){
         if (!self::$_instance instanceof self){
             self::$_instance = new self();
+            self::init();
         }
 
         return self::$_instance;
     }
 
-    public static function find($ip)
-    {
-        if (empty($ip) === TRUE)
-        {
+    public static function query($ip){
+        if (empty($ip) === TRUE){
             return 'N/A';
         }
         $nip   = gethostbyname($ip);
@@ -36,57 +41,84 @@ class Ip138{
         {
             return 'N/A';
         }
-        if (isset(self::$cached[$nip]) === TRUE)
-        {
-            return self::$cached[$nip];
-        }
-        if (self::$fp === NULL)
+        if (self::$db === NULL)
         {
             self::init();
         }
-        $nip = pack('N', ip2long($nip));
-        $tmp_offset = (int)$ipdot[0] * 4;
-        $start      = unpack('Vlen', self::$index[$tmp_offset] . self::$index[$tmp_offset + 1] . self::$index[$tmp_offset + 2] . self::$index[$tmp_offset + 3]);
-        $index_offset = $index_length = NULL;
-        $max_comp_len = self::$offset['len'] - 1028;
-        for ($start = $start['len'] * 8 + 1024; $start < $max_comp_len; $start += 8)
-        {
-            if (self::$index{$start} . self::$index{$start + 1} . self::$index{$start + 2} . self::$index{$start + 3} >= $nip)
-            {
-                $index_offset = unpack('Vlen', self::$index{$start + 4} . self::$index{$start + 5} . self::$index{$start + 6} . "\x0");
-                $index_length = unpack('Clen', self::$index{$start + 7});
-                break;
-            }
+        $end = 0;
+        $ip = ip2long($ip);
+        //使用 self.find 函数查找ip的索引偏移
+        if (($ip>>24)!=0xff){
+            $end = self::$index[($ip>>24)+1];
         }
-        if ($index_offset === NULL)
-        {
-            return 'N/A';
+        if ($end == 0){
+            $end = count(self::$ipEndAddr);
         }
-        fseek(self::$fp, self::$offset['len'] + $index_offset['len'] - 1024);
-        self::$cached[$nip] = explode("\t", fread(self::$fp, $index_length['len']));
-        return self::$cached[$nip];
+        $i = self::find($ip, self::$index[$ip>>24], $end);
+        $off = self::$textOffset[$i];
+        $addr = substr(self::$textData,$off,self::$textLen[$i]);
+        return  $addr;
+        //IP记录偏移值+4可以丢弃前4字节的IP地址信息。
+        //return self::$textData[off:off+self.textLen[i]]
     }
 
-    public function query(){
-
+    private static function find($ip,$left, $right){
+        //使用二分法查找网络字节编码的IP地址的索引记录
+        if ($right <= $left){
+            return $left;
+        }
+        $m = intval(($left + $right) / 2);
+        $new_ip = self::$ipEndAddr[$m];
+        if ($ip < $new_ip){
+            return self::find($ip, $left, $m-1);
+        }else{
+            return self::find($ip, $m+1, $right);
+        }
     }
+
+
+
     private static function init()
     {
-        if (self::$fp === NULL)
+        if (self::$db === NULL)
         {
-            self::$ip = new IP();
-            self::$fp = fopen(__DIR__ . '/ip.dat', 'rb');
-            if (self::$fp === FALSE)
+            self::$db = fopen(__DIR__ . '/../ip.dat', 'rb');
+            if (self::$db === FALSE)
             {
                 throw new Exception('Invalid ip.dat file!');
             }
-            self::$offset = unpack('Nlen', fread(self::$fp, 4));
-            if (self::$offset['len'] < 4)
-            {
-                throw new Exception('Invalid ip.dat file!');
-            }
-            self::$index = fread(self::$fp, self::$offset['len'] - 4);
+            self::read_data();
         }
+    }
+
+
+
+    private static function read_data(){
+        fseek(self::$db,0);
+        self::$idx_start = unpack('I', fread(self::$db, 4))[1];
+        fseek(self::$db,self::$idx_start);
+        self::$total = (self::$idx_start-4 -256*4) / 9;
+        while (!feof(self::$db)){
+            self::$textData.= fread(self::$db,4096);
+        }
+        fseek(self::$db,0);
+        for($i=0;$i<256;$i++){
+            $off = 4+4*$i;          
+           fseek(self::$db,$off);
+           self::$index[$i] =  unpack('I', fread(self::$db, 4))[1];
+        }
+
+        for($i=0;$i<self::$total;$i++){
+           $off = 4 + 1024 + $i*9;
+           fseek(self::$db,$off);
+           self::$ipEndAddr[$i] =  unpack('I', fread(self::$db, 4))[1];
+           fseek(self::$db,$off+4);
+           self::$textOffset[$i] =  unpack('I', fread(self::$db, 4))[1];
+           fseek(self::$db,$off+4+4);
+           self::$textLen[$i] =  unpack('C', fread(self::$db, 1))[1];
+        }
+
+        return self::$index;
     }
     public function __destruct()
     {
@@ -99,11 +131,11 @@ class Ip138{
 
 
 function main(){
-	$ip = '182.56.66.2';
-	$dbpath = '../ip.dat';
+	$ip = '202.194.48.39';
 	$ip138 = Ip138::getInstance();
 	$a = $ip138->query($ip);
-	sprintf('%15s %s',ip, a);
+	$output = sprintf('%15s %s',$ip, $a);
+    echo $output;
 }
 main();
 ?>
