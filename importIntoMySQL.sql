664031ba11e0fbc5050ba848f67395011cff6669
forthxu <forthxu@gmail.com>
2019-11-06 19:13:08 +0800

    update Mysql示例查询语句

--- importIntoMySQL.sql
+++ importIntoMySQL.sql
@@ -10,9 +10,11 @@ CREATE TABLE `ips` (
   `operator` varchar(64) NOT NULL,
   `zipcode` int(11) unsigned NOT NULL,
   `areacode` int(11) unsigned NOT NULL,
-  UNIQUE KEY `index` (`start`,`end`) USING BTREE
+  UNIQUE KEY `index` (`start`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #import into table ips from file ip_20191011091025.txt
 load data infile 'ip_20191011091025.txt' ignore into table ips character set utf8 fields terminated by ',' enclosed by '' lines terminated by '\n' (`start`,`end`,`startip`,`endip`,`country`,`province`,`city`,`operator`,`zipcode`,`areacode`);
 
+#ip location
+select * from ips WHERE start<=3748151430 order by start desc limit 1;
